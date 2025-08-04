8cc120e6304a67179bdb3af91ef35e5bd5f92095
jui-wen.chen <jui-wen.chen@useblocks.com>
2025-08-04 15:28:03 +0200

    normalized filepath for TC in different OS

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -247,8 +247,14 @@ def test_get_current_rev(git_repo: tuple[Path, str]) -> None:
 def test_analyzer(tmp_path, snapshot_anchors):
     src_dir = Path(__file__).parent.parent / "tests" / "data" / "s_core"
     anaylzer = SourceAnalyzer(src_dir, ["@req-id:"], outdir=tmp_path)
+    anaylzer.git_remote_url = None
+    anaylzer.git_commit_rev = None
     anaylzer.run()
+
     dumped_anchors = tmp_path / "anchors.json"
     with dumped_anchors.open("r") as f:
         anchors = json.load(f)
+    # normalize filepath
+    for anchor in anchors:
+        anchor["filepath"] = Path(anchor["filepath"]).as_posix()
     assert anchors == snapshot_anchors
