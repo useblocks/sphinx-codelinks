2f35424470932f90cd70ce127a43651ee3a81777
juiwenchen <jui-wen.chen@useblocks.com>
2025-08-12 16:43:00 +0200

    add CLI for needextend bridge

--- tests/test_bridge.py
+++ tests/test_bridge.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
-from sphinx_codelinks.bridge import convert_makred_content
+from sphinx_codelinks.needextend_bridge import convert_marked_content
 
 
-def test_bridge():
+def test_bridge(tmp_path):
     jsonpath = Path(__file__).parent / "data" / "analyse" / "marked_content.json"
-    convert_makred_content(jsonpath, outdir=Path(__file__).parent.parent / "output")
+    convert_marked_content(jsonpath, outdir=tmp_path)
+    needextend_path = tmp_path / "needextend.rst"
+
+    assert needextend_path.exists()
